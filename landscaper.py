97d8ebeaa70f1afd1e335a3db20c93d43ba08d4a
Cindy Wong <cindywongdev@gmail.com>
2023-02-01 21:16:39 -0500

    user can buy a battery-powered lawnmower

--- landscaper.py
+++ landscaper.py
@@ -1,10 +1,11 @@
 money = 0
 hasScissors = False
-hasLawnMower = False
+hasPushLawnMower = False
+hasBatPowLawnMower = False
 
 while(True):
     
-    user_choice = input("[1] Cut Lawns with Teeth [2] Buy Scissors ($5) [3] Cut Lawns with Scissors [4] Buy Push Lawnmower ($25) [5] Cut Lawns with Push Lawnmower  [10] Check Money ")
+    user_choice = input("[1] Cut Lawns with Teeth [2] Buy Scissors ($5) [3] Cut Lawns with Scissors [4] Buy Push Lawnmower ($25) [5] Cut Lawns with Push Lawnmower [6] Buy Battery-Powered Lawnmower ($250) [10] Check Money ")
     
     if (user_choice == "1"):
         money += 1
@@ -33,23 +34,35 @@ while(True):
             
     if (user_choice == "4"):
         # check if user alread has lawnmower
-        if (hasLawnMower == True):
+        if (hasPushLawnMower == True):
             print("You already have a push lawnmower.\n")
         # check if user has enough money
         elif (money >= 25):
             money -= 25
-            hasLawnMower = True
+            hasPushLawnMower = True
             print("You bought a push lawnmower! Now you can cut lawns with it.\n")
         else:
             print("Not enough money. Cut more lawns!\n")
             
     if (user_choice == "5"):
         # check if user has lawnmower
-        if (hasLawnMower == True):
+        if (hasPushLawnMower == True):
             money += 50
-            print(f"You cut lawns with the push lawnmower and earned $50! Total money: ${money}")
+            print(f"You cut lawns with the push lawnmower and earned $50! Total money: ${money}\n")
         else:
             print("You don't have a push lawnmower yet. Buy one first!\n")
+            
+    if (user_choice == "6"):
+        # check if user has it already
+        if (hasBatPowLawnMower == True):
+            print("You already have a battery-powered lawnmower.\n")
+        # check if user has enough money
+        elif (money >= 250):
+            money -= 250
+            hasBatPowLawnMower = True
+            print("You bought a battery-powered lawnmower! Now you can cut lawns with it.\n")
+        else:
+            print("Not enough money. Cut more lawns!\n")
             
     if (user_choice == "10"):
         print(f"Total money: ${money}")
